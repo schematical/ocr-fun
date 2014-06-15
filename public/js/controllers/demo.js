83bff402db2abf70960704270b76adb79857c180
Matt Lea <mlea@schematical.com>
2014-06-15 12:40:17 -0500

    s3 problems

--- public/js/controllers/demo.js
+++ public/js/controllers/demo.js
@@ -9,9 +9,9 @@ angular.module('iraas', [])
             '$scope',
             function($scope) {
                 //Get image dimensions
-                var jImage = $('img-demo');
+                var jImage = $('#img-demo');
 
-
+                var jDemoHolder = $('#div-demo-holder');
 
 
                 $("<img/>") // Make in memory copy of image to avoid css issues
@@ -19,12 +19,30 @@ angular.module('iraas', [])
                     .load(function() {
                         var pic_real_width = this.width;   // Note: $(this).width() will not
                         var pic_real_height = this.height; // work for in memory images.
-
+                        var h_ratio = pic_real_width/parseInt(jImage.css('width'));
+                        var v_ratio = pic_real_height/parseInt(jImage.css('height'));
                         //Load up the bootstraped content
                         for(var i in njax_bootstrap.clusters){
                             //Render new boxes using the bounds
-                            njax_bootstrap.clusters[i]
-                            $()
+                            var bounds = njax_bootstrap.clusters[i].bounds;
+
+                            var scale = .1;
+                            var left = (bounds.cols.min/scale) / h_ratio;
+                            var width = ((bounds.cols.max - bounds.cols.min)/scale) / h_ratio;
+
+                            var top = (bounds.rows.min/scale) / v_ratio;
+                            var height = ((bounds.rows.max - bounds.rows.min)/scale) / v_ratio;
+
+                            var jFrame = $("<div></div>");
+                            //TODO Move to CSS File
+                            jFrame.css('border', 'thin red solid');
+                            jFrame.css('position', 'absolute');
+                            jFrame.css('top', top);
+                            jFrame.css('left', left);
+                            jFrame.css('width', width);
+                            jFrame.css('height', height);
+                            jDemoHolder.append(jFrame);
+                            console.log(jDemoHolder);
                         }
                 });
                 $scope.enter = function(){
